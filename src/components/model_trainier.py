fbbfe5367763b5d1d095d4245dbef9f8d91edaff
atulalways2 <atultewari26always@gmail.com>
2026-02-13 14:47:49 +0530

    added model_trainer and customised accordingly in the module files

--- src/components/model_trainier.py
+++ src/components/model_trainier.py
@@ -17,5 +17,69 @@ from xgboost import XGBRegressor
 # custom libaries import
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object
+from src.utils import save_object,evaluate_models
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+
+    def initiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Splitting training and test input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+
+            models ={
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting":GradientBoostingRegressor(),
+                "LinearRegression": LinearRegression(),
+                "K-Neighbors Classifier": KNeighborsRegressor(),
+                "XG Boost": XGBRegressor(),
+                "CatBoostRegressor": CatBoostRegressor(),
+                "AdaBoost Classifier": AdaBoostRegressor(),
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models)
+            
+            # to get the best model score form the dict
+            best_model_score=max(sorted(model_report.values()))
+
+            #to get best model name from dict
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            #Setting a benchmark for the accuracy score:
+            if best_model_score < 0.6:
+                raise CustomException ("No best Model found!")
+            
+            logging.info(f"Best found model on both training and test dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+            r2 = r2_score(y_test,predicted)
+            return r2
+        
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
 
